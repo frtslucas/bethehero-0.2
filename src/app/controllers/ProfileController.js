1c43028583724ed22e5ed4d4a2824f636e95b313
Lucas Furtoso <lucas.furtoso@gmail.com>
2020-03-31 13:12:58 -0300

    Added token validation using authMiddleware

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -3,10 +3,8 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.use(authMiddleware);
-
-router.get('/', (request, response) => {
-    response.send({ ok: true, user: request.userId });
-});
-
-module.exports = (app) => app.use('/profile', router)
+module.exports = {
+    async index (request, response) {
+        response.send({ ok: true, user: request.userId });
+    }
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
+const ProfileController = require('./controllers/ProfileController');
+const authMiddleware = require('./middlewares/authMiddleware');
 
 const routes = express.Router();
 
@@ -7,5 +9,9 @@ routes.post('/register', SessionController.register);
 routes.post('/login', SessionController.login);
 routes.post('/forgot-password', SessionController.forgotPassword);
 routes.post('/reset-password', SessionController.resetPassword);
+
+routes.use(authMiddleware);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
